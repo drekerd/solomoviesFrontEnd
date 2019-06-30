8074c39e8848dc33985184e3d953b727ffac43b8
Mário Silva <mariosilva@Marios-MacBook-Pro.local>
2019-06-30 23:46:48 +0100

    smallChanges

--- src/main/java/com/rumos/rumosthemoviefreemarker/newMovie/NewMovieController.java
+++ src/main/java/com/rumos/rumosthemoviefreemarker/newMovie/NewMovieController.java
@@ -2,13 +2,13 @@ package com.rumos.rumosthemoviefreemarker.newMovie;
 
 import com.rumos.rumosthemoviefreemarker.newMovie.dao.MovieRepoDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -25,45 +25,54 @@ public class NewMovieController {
     private MovieRepoDao movieRepo;
 
     @RequestMapping("/add-movie")
-    public ModelAndView addNewMovie(){
+    public ModelAndView addNewMovie() {
         return new ModelAndView("addNewMovie");
     }
 
-    @RequestMapping(value="/add-movie", method = RequestMethod.POST)
+    @RequestMapping(value = "/add-movie", method = RequestMethod.POST)
     public void setHello(NewMovie movie) {
         System.out.println(movie);
         this.movies.add(movie);
     }
 
-    @RequestMapping(value="/admin/add-new-movie")
-    public ResponseEntity<?> addNewMoviePost(@RequestBody NewMovie movie){
+    @RequestMapping(value = "/admin/add-new-movie")
+    public ResponseEntity<?> addNewMoviePost(@RequestBody NewMovie movie) {
         System.out.println(movie.toString());
-        movieRepo.save(movie);
-        return ResponseEntity.ok(movie);
+        return checkIfMovieExist(movie);
     }
 
     @GetMapping("admin/list-movies")
     public ModelAndView getMovies(Model model) {
         this.movies = movieRepo.findAll();
         System.out.println(movieRepo.findAll());
-        model.addAttribute("moviesFromDB",this.movies);
+        model.addAttribute("moviesFromDB", this.movies);
         return new ModelAndView("admin");
     }
 
-    @PutMapping(path="/admin/edit-movie", consumes = {"application/json"})
-    public ResponseEntity<?> updateMovie(@RequestBody NewMovie movie){
+    @PutMapping(path = "/admin/edit-movie", consumes = {"application/json"})
+    public ResponseEntity<?> updateMovie(@RequestBody NewMovie movie) {
         System.out.println(movie.toString());
         movieRepo.save(movie);
         return ResponseEntity.ok(movieRepo.findById(movie.getId()));
     }
 
-    @DeleteMapping(path="/admin/delete-movie/{id}")
-    public ResponseEntity<?> deleteMovie(@PathVariable("id") long id){
+    @DeleteMapping(path = "/admin/delete-movie/{id}")
+    public ResponseEntity<?> deleteMovie(@PathVariable("id") long id) {
         System.out.println(id);
         movieRepo.deleteById(id);
 
         return ResponseEntity.ok(id);
+    }
 
+    private ResponseEntity<?> checkIfMovieExist(NewMovie movie){
+
+        this.movies = movieRepo.findAll();
+        if (this.movies.contains(movie)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Duplicated Entry");
+        } else {
+            movieRepo.save(movie);
+            return ResponseEntity.ok(movie);
+        }
 
     }
 }
--- src/main/resources/static/js/updateMovie.js
+++ src/main/resources/static/js/updateMovie.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#sub").click(function(){
-    var year = {"year":$("#exampleInputEmail1").val()};
+    $("#sub").click(function () {
+        var year = {"year": $("#exampleInputEmail1").val()};
         $.ajax({
-            url:"getBestMovies",
-            data : year,
-            success: function(msg){
+            url: "getBestMovies",
+            data: year,
+            success: function (msg) {
                 window.location.reload();
             }
         });
@@ -14,10 +14,10 @@ $(document).ready(function(){
 
     $(".editbtn").on('click', function () {
 
- console.log("clicked edit");
+        console.log("clicked edit");
 
         $('#modalEditForm').modal({
-            show:true,
+            show: true,
             backdrop: false
         });
 
@@ -29,13 +29,13 @@ $(document).ready(function(){
             return $(this).text();
         }).get();
 
-         console.log(movie);
+        console.log(movie);
 
-        var movieImage =$row.children('td').find('.poster').attr('src');
+        var movieImage = $row.children('td').find('.poster').attr('src');
 
 
-        console.log("Movie From Grid "+movie);
-        console.log("Movie Image "+$row.children('td').find('.poster').attr('src'));
+        console.log("Movie From Grid " + movie);
+        console.log("Movie Image " + $row.children('td').find('.poster').attr('src'));
 
         $('#mid').val(movie[0]);
         $('#mName').val(movie[2]);
@@ -50,22 +50,22 @@ $(document).ready(function(){
         console.log("entered in edit");
 
         var newMovie = {
-            "id":$("#mid").val(),
-            "name":$("#mName").val(),
-            "date":$("#mReleaseDate").val(),
-            "thumbnailFullLink":$("#mImage").val(),
+            "id": $("#mid").val(),
+            "name": $("#mName").val(),
+            "date": $("#mReleaseDate").val(),
+            "thumbnailFullLink": $("#mImage").val(),
             "description": $("#mDescription").val()
         };
 
         console.log(newMovie);
 
         $.ajax({
-            type:'PUT',
-            contentType:'application/json',
-            dataType:'json',
-            url:"/admin/edit-movie",
-            data:JSON.stringify(newMovie),
-            success:function (response) {
+            type: 'PUT',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/admin/edit-movie",
+            data: JSON.stringify(newMovie),
+            success: function (response) {
                 console.log(response);
                 $('#modalEditForm').modal('hide');
                 window.location.reload();
@@ -79,69 +79,68 @@ $(document).ready(function(){
         window.location.reload();
     });
 
+    //add Movie
     $("#addButton").on('click', function () {
 
-            $('#modalAddForm').modal({
-                show:true,
-                backdrop: false
-            });
+        $('#modalAddForm').modal({
+            show: true,
+            backdrop: false
+        });
     });
 
+    $("#movieAddForm").on('submit', function (e) {
 
+        e.preventDefault();
 
-     $("#movieAddForm").on('submit',function (e) {
-
-         e.preventDefault();
-
-         var newMovie = {
-             "id":$("#mAddid").val(),
-             "name":$("#mAddName").val(),
-             "date":$("#mAddReleaseDate").val(),
-             "thumbnailFullLink":$("#mAddImage").val(),
-             "description": $("#mAddDescription").val()
-         };
+        var newMovie = {
+            "id": $("#mAddid").val(),
+            "name": $("#mAddName").val(),
+            "date": $("#mAddReleaseDate").val(),
+            "thumbnailFullLink": $("#mAddImage").val(),
+            "description": $("#mAddDescription").val()
+        };
 
         $.ajax({
 
-             type:"POST",
-             contentType:'application/json',
-             dataType:'json',
-             url:'/admin/add-new-movie',
-             data: JSON.stringify(newMovie),
-             success: function (result) {
-                 window.location='/admin/list-movies'
-                 var json = JSON.stringify(result);
-                 $('#feedback').html(json);
-                  window.location.reload();
-             }
-         });
+            type: "POST",
+            contentType: 'application/json',
+            dataType: 'json',
+            url: '/admin/add-new-movie',
+            data: JSON.stringify(newMovie),
+            success: function (result) {
+                window.location = '/admin/list-movies'
+                var json = JSON.stringify(result);
+                $('#feedback').html(json);
+                window.location.reload();
+            }
+        });
 
-     });
+    });
 
-     //Delete
-     $(".deletebtn").on('click', function (e){
+    //Delete
+    $(".deletebtn").on('click', function (e) {
         e.preventDefault();
 
         var $row = $(this).closest('tr');
 
-         var movie = $row.children('td').map(function () {
-                    return $(this).text();
-            }).get();
+        var movie = $row.children('td').map(function () {
+            return $(this).text();
+        }).get();
 
-         var id = movie[0];
+        var id = movie[0];
 
-         $.ajax({
+        $.ajax({
 
-                     type:"DELETE",
-                     url:'/admin/delete-movie/'+id,
-                     success: function (result) {
-                         window.location='/admin/list-movies'
-                         var json = JSON.stringify(result);
-                          window.location.reload();
+            type: "DELETE",
+            url: '/admin/delete-movie/' + id,
+            success: function (result) {
+                window.location = '/admin/list-movies'
+                var json = JSON.stringify(result);
+                window.location.reload();
 
-                     }
-                 });
+            }
         });
+    });
 
 
 });
